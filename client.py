41dff4f73a2608aee108a64bf1500feda4bd8285
aHorovitz <Horovitz.as@gmail.com>
2024-11-18 07:54:22 +0200

    Add zone txt, change the argv2 and print decode

--- client.py
+++ client.py
@@ -6,8 +6,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
     #ask for a domain's IP
     message=input()
-    s.sendto(message.encode(), (sys.argv[1], sys.argv[2]))
+    s.sendto(message.encode(), (sys.argv[1], int(sys.argv[2])))
     #wait for an answer and print it
     data, addr = s.recvfrom(1024)
-    print(str(data))
+    print(data.decode())
 s.close()
